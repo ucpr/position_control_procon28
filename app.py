ec8b4bb4a667dc5a4769c70ad915f780af7fca46
nve3pd <uchihara1229@gmail.com>
2017-08-21 20:47:18 +0900

    implemented color tracking

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env/python3
 
+from collections import deque
+import imutils
 import cv2
 import numpy as np
+
+LOWER_BLUE = np.array([110, 50, 50])
+UPPER_BLUE = np.array([130, 255, 255])
+pts = deque(maxlen=64)
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -16,23 +22,40 @@ def main():
         #HSV色空間に変換
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        lower_blue = np.array([110, 50, 50])
-        upper_blue = np.array([130, 255, 255])
-
         #色検出
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        mask = cv2.inRange(hsv, LOWER_BLUE, UPPER_BLUE)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
-        res = cv2.bitwise_and(frame, frame, mask=mask)
+        #輪郭の抽出
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        center = None
+
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            if radius > 10:
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+        pts.appendleft(center)
+
+#        for i in range(1, len(pts)):
+#            if pts[i - 1] is None or pts[i] is None:
+#                continue
+
+#            thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+#            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
         cv2.imshow("frame", frame)
-#        cv2.imshow("mask", mask)
-        cv2.imshow("res", res)
 
         if cv2.waitKey(5) & 0xFF == 27:
             break
 
+    cap.release()
     cv2.destroyAllWindows()
- 
 
 if __name__ == "__main__":
     main()
