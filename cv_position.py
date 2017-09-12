61ee7293cc8cf8123d83d69180a912366ff6bdab
nve3pd <uchihara1229@gmail.com>
2017-09-12 23:29:37 +0900

    positionを呼び出すことで値を返すようにした

--- cv_position.py
+++ cv_position.py
@@ -9,6 +9,7 @@ LOWER_BLUE = np.array([110, 50, 50])
 UPPER_BLUE = np.array([130, 255, 255])
 pts = deque(maxlen=64)
 
+
 def diff(a, b):
     """
     a : tuple # (width, height)
@@ -26,7 +27,7 @@ def ptoc(w, h, dpi=72):
 
     return width, height
 
-def main():
+def position():
     cap = cv2.VideoCapture(0)
     
     if cap.isOpened() is False:
@@ -34,49 +35,50 @@ def main():
 
     cv2.namedWindow("Capture", cv2.WINDOW_AUTOSIZE)
 
-    while True:
-        _, frame = cap.read()
-        frame = imutils.resize(frame, width=640, height=480)
-        height, width = frame.shape[:2]
+    _, frame = cap.read()
+    frame = imutils.resize(frame, width=640, height=480)
+    height, width = frame.shape[:2]
 
-        #HSV色空間に変換
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #HSV色空間に変換
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        #色検出
-        mask = cv2.inRange(hsv, LOWER_BLUE, UPPER_BLUE)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
+    #色検出
+    mask = cv2.inRange(hsv, LOWER_BLUE, UPPER_BLUE)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
 
-        #輪郭の抽出
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
+    #輪郭の抽出
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
 
-        if len(cnts) > 0:
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            if radius > 10:
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-        pts.appendleft(center)
+    pts.appendleft(center)
 
-        cv2.circle(frame, (width // 2, height // 2), 5, (255, 0, 0), 10)
+    cv2.circle(frame, (width // 2, height // 2), 5, (255, 0, 0), 10)
 
-        if center is not None:
-            w, h = diff((width // 2, height // 2), center)
-            print("diff", "width:", w, "height:", h)
-            w, h = ptoc(w, h)
-            print("pixel to cm", "width:", w, "height:", h)
+    if center is not None:
+        w, h = diff((width // 2, height // 2), center)
+        w, h = ptoc(w, h)
+    else:
+        w, h = None, None
 
-        cv2.imshow("frame", frame)
-
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
+    #while True:
+    #    cv2.imshow("frame", frame)
+    #    if cv2.waitKey(5) & 0xFF == 27:
+    #        break
 
     cap.release()
     cv2.destroyAllWindows()
+
+    return w, h 
 
 if __name__ == "__main__":
     main()
