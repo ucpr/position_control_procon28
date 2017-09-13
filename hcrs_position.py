059d71b35d504b21f8c40b0dc111e5f5f4abafb8
nve3pd <uchihara1229@gmail.com>
2017-09-13 17:51:53 +0900

    関数を呼び出すことで値を返すようにした

--- hcrs_position.py
+++ hcrs_position.py
@@ -10,30 +10,28 @@ ECHO1 = 13
 GPIO.setup(TRIG1,GPIO.OUT)
 GPIO.setup(ECHO1,GPIO.IN)
 
-def reading(sensor):
-    if sensor == 0:
-        GPIO.output(TRIG1, GPIO.LOW)
-        time.sleep(0.3)
+def position():
+    GPIO.output(TRIG1, GPIO.LOW)
+    time.sleep(0.3)
        
-        GPIO.output(TRIG1, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG1, False)
+    GPIO.output(TRIG1, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG1, False)
 
-        signaloff = 0
-        signalon  = 0
+    signaloff = 0
+    signalon  = 0
  
-        while  GPIO.input(ECHO1) == 0:
-          signaloff = time.time()
+    while  GPIO.input(ECHO1) == 0:
+        signaloff = time.time()
          
-        while GPIO.input(ECHO1) == 1:
-          signalon = time.time()
- 
-        timepassed = signalon - signaloff
-        distance = timepassed * 17000
-        return distance
-        GPIO.cleanup()
-    else:
-        print ("Incorrect usonic() function varible.")
+    while GPIO.input(ECHO1) == 1:
+        signalon = time.time()
 
-while True:
-    print(reading(0))
+    timepassed = signalon - signaloff
+    distance = timepassed * 17000
+    return distance
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    while True:
+        print(reading())
